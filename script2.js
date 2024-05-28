5f70ca2b0b0aeac656d0188a5a857cf6ec334d5c
rishitgondaliya <mrrishitgondaliya@gmail.com>
2024-05-28 22:59:52 +0530

    add files

---
+++ script2.js
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+// JavaScript code for handling form submission
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("enquiry-form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // You can implement the form submission logic here,
+        // such as sending data to a server or displaying a confirmation message.
+        
+        // For demonstration purposes, let's display an alert message.
+        alert("Form submitted successfully!");
+    });
+});
+=======
+// JavaScript code for handling form submission
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("enquiry-form");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // You can implement the form submission logic here,
+        // such as sending data to a server or displaying a confirmation message.
+        
+        // For demonstration purposes, let's display an alert message.
+        alert("Form submitted successfully!");
+    });
+});
+>>>>>>> d925270 (Initial commit)
